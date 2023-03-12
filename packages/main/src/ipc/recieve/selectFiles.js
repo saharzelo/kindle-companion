e27049ef2f69614a95c4c446a5cfbe7b2415fb9e
sahar <saharzelonagora@gmail.com>
2023-03-12 19:48:29 +0200

    refactored main ipc

--- packages/main/src/ipc/recieve/selectFiles.js
+++ packages/main/src/ipc/recieve/selectFiles.js
@@ -1,20 +1,10 @@
 // Listen for the "select-file" message from the renderer process
-
+import { sendFiles } from '../send/sendFiles';
 const { dialog, ipcMain } = require('electron');
 const fs = require('fs');
 const path = require("path");
 
 
 ipcMain.on('selectFiles', async (event) => {
-	const options = { title: 'Select a file', properties: ['openDirectory'] };
-	dialog.showOpenDialog(options).then((result) => {
-		if (!result.canceled) {
-		  const folderPath = result.filePaths[0];
-		  const filePath = folderPath + '/system/vocabulary/vocab.db';
-		  event.sender.send('file-contents', filePath);
-      console.log('helo')
-		}
-	  }).catch((err) => {
-		console.log(err);
-	  });
+  sendFiles(event)
 });
--- packages/preload/src/ipc/recieve/fileListener.js
+++ packages/preload/src/ipc/recieve/fileListener.js
@@ -2,9 +2,7 @@ import { ipcRenderer } from 'electron';
 
 const sqlite3 = require('sqlite3').verbose();
 
-ipcRenderer.on('file-contents', (event, data) => {
-  console.log('here')
-  console.log(`Selected file: ${data}`);
+ipcRenderer.on('fileContent', (event, data) => {
   const db = new sqlite3.Database(data);
   db.all('SELECT title FROM BOOK_INFO', [], (err, rows) => {
     if (err) {
@@ -15,4 +13,3 @@ ipcRenderer.on('file-contents', (event, data) => {
 })
 
 })
-
