e30f6fdccc0c62c12e102baf85cbb99ba91d67d2
sahar <saharzelonagora@gmail.com>
2023-03-25 20:55:24 +0200

    new design

--- packages/renderer/src/view/components/BookItem/BookItem.jsx
+++ packages/renderer/src/view/components/BookItem/BookItem.jsx
@@ -2,12 +2,19 @@ import React from 'react';
 import './BookItem.css';
 
 function BookPreviewItem({ imageSrc, title, text }) {
-    let c = 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/416Xx903tRL.jpg'
+  let c = 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/416Xx903tRL.jpg'
   return (
     <div className="book-preview-item">
-      <img id='book-thumnail' src={c} alt={title}/>
-      <h6>{title}</h6>
-      <p>Words: Clippings:</p>
+      <div className='book-thumbnail'>
+        <img id='book-thumnail' src={c} alt={title} />
+      </div>
+      <div className="book-title">
+        <h6>{title}</h6>
+      </div>
+      <div className="book-info">
+        <span>Vocabulary: 54</span> <span>Clippings: 22</span>
+      </div>
+
     </div>
   );
 }
