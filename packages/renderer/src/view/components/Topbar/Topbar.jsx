d5b81eb73bf17222b5781c2afaabf8582a38613b
sahar <saharzelonagora@gmail.com>
2023-03-26 04:48:11 +0300

    Added topbar

--- packages/renderer/src/view/components/Topbar/Topbar.jsx
+++ packages/renderer/src/view/components/Topbar/Topbar.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import './Topbar.css';
+import Dropdown from '../Dropdown/Dropdown';
+import { useState } from 'react';
 
 function Topbar() {
+
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <div className="Topbar">
+      <div className="user-info">
+        Welcome: Shay Ventura
+      </div>
+      <div>
+        <Dropdown isOpen={isOpen} toggleDropdown={toggleDropdown} />
+      </div>
+
     </div>
   );
 }
