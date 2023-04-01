271def7df751dbab5cf7e3f59f9ad36b2c2905af
sahar <saharzelonagora@gmail.com>
2023-04-01 15:57:33 +0300

    removed blob

--- packages/renderer/src/App.jsx
+++ packages/renderer/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import LoginPage from './view/pages/LoginPage/LoginPage'
+import HomePage from './view/pages/HomePage/HomePage';
 import LibraryPage from './view/pages/LibraryPage/LibraryPage';
 
 function App() {
@@ -9,34 +10,6 @@ function App() {
     setProfile(profile);
   }
 
-  // useEffect(() => {
-  //   const checkKindleStatus = async () => {
-  //     try {
-  //       // call function that chcks if files exists
-  //       if (response.ok) {
-  //         setIsLoggedIn(true);
-  //       } else {
-  //         throw new Error('Not logged in');
-  //       }
-  //     } catch (error) {
-  //       console.error(error);
-  //     }
-  //   };
-  //   checkKindleStatus();
-  // }, []);
-
-  // const handleLogin = async () => {
-  //   try {
-  //     // call backend to see if files exists
-  //     if (response.ok) {
-  //       // set state as the profile
-  //     } else {
-  //       throw new Error('Invalid credentials');
-  //     }
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // }
   return (
     <div className="App">
       {profile ? (
