40ee1a63257fbeb0cbb34b0cf57d5ef6840fcbcb
sahar <saharzelonagora@gmail.com>
2023-04-01 19:02:31 +0300

    added functionality to sidebar

--- packages/renderer/src/view/components/Sidebar/Sidebar.jsx
+++ packages/renderer/src/view/components/Sidebar/Sidebar.jsx
@@ -5,18 +5,26 @@ import { ReactComponent as AppIcon } from '../../../public/icons/loginIcon.svg'
 import { ReactComponent as BookSvg } from '../../../public/icons/Sidebar/book.svg'
 import { ReactComponent as HomeSvg } from '../../../public/icons/Sidebar/home.svg'
 import { ReactComponent as SettingsSvg } from '../../../public/icons/Sidebar/settings.svg'
-function Sidebar() {
+function Sidebar({ setPage, page }) {
+
+  const handleSetPage = (curPage) => {
+    setPage(curPage)
+  }
   return (
     <div className="sidebar">
       <div className="app-logo"><AppIcon /></div>
-
-      <div className="button-group">
+      <div
+        className={`button-group ${page === null ? 'active' : ''}`}
+        onClick={() => { handleSetPage(null) }}>
         <HomeSvg />
       </div>
-      <div className="button-group">
+      <div
+        className={`button-group ${page === 'library' ? 'active' : ''}`}
+        onClick={() => { handleSetPage('library') }}>
         <BookSvg />
       </div>
-      <div className="button-group">
+      <div
+        className={`button-group ${page === 'settings' ? 'active' : ''}`}>
         <SettingsSvg />
       </div>
     </div>
