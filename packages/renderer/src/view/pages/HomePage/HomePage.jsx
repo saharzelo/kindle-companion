578d066dd2970bc8d6a641d7a4bb5fc4b08b58ff
sahar <saharzelonagora@gmail.com>
2023-04-01 15:24:54 +0300

    scaffold

---
+++ packages/renderer/src/view/pages/HomePage/HomePage.jsx
@@ -0,0 +1,15 @@
+import './HomePage.css'
+import Sidebar from '../../components/Sidebar/Sidebar'
+import Topbar from '../../components/Topbar/Topbar'
+
+
+function HomePage({}) {
+    return (
+        <div className="home-page">
+
+        </div>
+    )
+}
+
+
+export default HomePage
