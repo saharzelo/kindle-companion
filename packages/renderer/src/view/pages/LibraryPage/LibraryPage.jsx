80ee6cd81dfade26a5222372b893e403e530f0b4
sahar <saharzelonagora@gmail.com>
2023-04-01 22:57:46 +0300

    added bookInfo modal

--- packages/renderer/src/view/pages/LibraryPage/LibraryPage.jsx
+++ packages/renderer/src/view/pages/LibraryPage/LibraryPage.jsx
@@ -1,12 +1,18 @@
 import { useState, useEffect } from 'react';
-import Sidebar from '../../components/Sidebar/Sidebar';
-import Topbar from '../../components/Topbar/Topbar';
 import { vocabDbRepo, getThumbnails } from '#preload';
 import './LibraryPage.css';
 import BookCatalogItem from '../../components/BookItem/BookCatalogItem';
+import BookInfo from '../../components/BookInfo/BookInfo';
 
 function LibraryPage({ profile }) {
-    const [bookCatalog, SetBookCatalog] = useState([]);
+    const [bookCatalog, setBookCatalog] = useState([]);
+    const [selectedBook, setSelectedBook] = useState(null);
+    const [showModal, setShowModal] = useState(false);
+
+    const handleBookClick = () => {
+        console.log('baby?!')
+        setShowModal(true);
+    }
 
     useEffect(() => {
         async function prepKindleData() {
@@ -15,22 +21,19 @@ function LibraryPage({ profile }) {
                 const bookMetadata = await vocabRepo.findAll();
                 const bookId = bookMetadata.map((result) => (result.asin));
                 const bookThumbnails = await getThumbnails(bookId)
-
                 const bookCatalog = bookMetadata.map(({ title, asin }, index) => (
-                    <BookCatalogItem key={index} title={title} thumbnail={bookThumbnails[asin]} />
+                    <BookCatalogItem key={index} title={title} thumbnail={bookThumbnails[asin]} onClick={() => handleBookClick()} />
                 ));
-
-                SetBookCatalog(bookCatalog);
+                setBookCatalog(bookCatalog);
             } catch (error) {
                 console.error(error);
-            }
+            };
         }
         prepKindleData();
     }, []);
 
     return (
         <div className="catalog-wrapper">
-
             <div className="catalog-header">
                 <h3>Your Library: </h3>
             </div>
@@ -38,7 +41,7 @@ function LibraryPage({ profile }) {
             <div className="catalog-container">
                 {bookCatalog}
             </div>
-
+            <BookInfo book={selectedBook} showModal={showModal} setShowModal={setShowModal} />
         </div>
     );
 }
