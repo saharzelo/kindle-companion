69a43d36819ef90b48a4eefc06837134fff4a2da
sahar <saharzelonagora@gmail.com>
2023-04-01 23:20:57 +0300

    improved ternerary rendering

--- packages/renderer/src/App.jsx
+++ packages/renderer/src/App.jsx
@@ -1,31 +1,32 @@
 import { useState, useEffect } from 'react';
-import './App.css'
-import LoginPage from './view/pages/LoginPage/LoginPage'
+import './App.css';
+import LoginPage from './view/pages/LoginPage/LoginPage';
 import Sidebar from './view/components/Sidebar/Sidebar';
 import Topbar from './view/components/Topbar/Topbar';
 import HomePage from './view/pages/HomePage/HomePage';
 import LibraryPage from './view/pages/LibraryPage/LibraryPage';
-import SettingsPage from './view/pages/SettingsPage/SettingsPage'
+import SettingsPage from './view/pages/SettingsPage/SettingsPage';
 
 function App() {
-  const [kindleCon, setkindleCon] = useState(null);
+  const [kindleSynced, setKindleSynced] = useState(null);
   const [page, setPage] = useState(null);
 
   function handleProfile(kindleCon) {
-    setkindleCon(kindleCon);
+    setKindleSynced(kindleCon);
   }
   return (
     <div className="App">
-      <Sidebar setPage={setPage} page={page}/>
+      <Sidebar setPage={setPage} page={page} />
       <div className="main-container">
         <Topbar />
-        {kindleCon ? (
-          page === 'library' ? (<LibraryPage profile={kindleCon} />) : (
-            page === 'settings' ? (<SettingsPage />) : (<HomePage />)
-          )
-        ) : (
-          <LoginPage setProfile={handleProfile} />
-        )}
+        {kindleSynced
+          ? page === 'library'
+            ? <LibraryPage profile={kindleSynced} />
+            : page === 'settings'
+              ? <SettingsPage />
+              : <HomePage />
+          : <LoginPage setProfile={handleProfile} />
+        }
       </div>
     </div>
   );
--- packages/renderer/src/view/pages/HomePage/HomePage.jsx
+++ packages/renderer/src/view/pages/HomePage/HomePage.jsx
@@ -3,16 +3,15 @@ import Sidebar from '../../components/Sidebar/Sidebar'
 import Topbar from '../../components/Topbar/Topbar'
 
 
-function HomePage({}) {
+function HomePage({ }) {
     return (
         <div className="home-page">
             <div className="catalog-header">
-                <h3>Recent Overview: </h3>
+                <h3> Your Overview: </h3>
             </div>
 
         </div>
     )
 }
 
-
-export default HomePage
+export default HomePage;
