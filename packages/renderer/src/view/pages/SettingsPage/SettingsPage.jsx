f2a79ec3d58343f9f0159bfe663b4d07be73e87e
sahar <saharzelonagora@gmail.com>
2023-04-01 19:33:00 +0300

    settings page scaffold

---
+++ packages/renderer/src/view/pages/SettingsPage/SettingsPage.jsx
@@ -0,0 +1,15 @@
+import './SettingsPage.css'
+
+function SettingsPage({}) {
+    return (
+        <div className="settings-page">
+            <div className="catalog-header">
+                <h3>Settings: </h3>
+            </div>
+
+        </div>
+    )
+}
+
+
+export default SettingsPage
