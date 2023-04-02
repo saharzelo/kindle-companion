90082790624a3f6929dbcc53e77a28d844dd947e
sahar <saharzelonagora@gmail.com>
2023-04-02 13:32:27 +0300

    improved book card/modal

--- packages/renderer/src/view/components/BookInfo/BookInfo.jsx
+++ packages/renderer/src/view/components/BookInfo/BookInfo.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './BookInfo.css';
+import myImage from './pic.jpg';
 
 function BookInfo({ book, showModal, setShowModal }) {
     const handleCloseModal = () => setShowModal(false);
@@ -9,14 +10,36 @@ function BookInfo({ book, showModal, setShowModal }) {
             {showModal && (
                 <>
                     <div className="book-info-modal-bg" onClick={handleCloseModal}></div>
-                        <div className="modal-content">
-                            <div className="modal-toolbar">
-                                <span className="close" onClick={handleCloseModal}>&times;</span>
-                            </div>
-                            <h2>Anna</h2>
-                            <img />
-                            <p>Awesome</p>
+
+
+                    <div className="modal-content">
+
+                        <div className="modal-header">
+                            <span className="close" onClick={handleCloseModal}>&times;</span>
                         </div>
+
+                        <div className="modal-body">
+                            <div className="book-cover">
+                                <img src={myImage} />
+
+                            </div>
+
+                            <div className="book-thumbnail">
+                                <img src={myImage} /> hello 
+                            </div>
+
+                            <div className="book-info">
+                                HEllo
+                            </div>
+
+                        </div>
+
+                        <div className="modal-footer">
+
+                        </div>
+
+                    </div>
+
                 </>
             )}
         </>
