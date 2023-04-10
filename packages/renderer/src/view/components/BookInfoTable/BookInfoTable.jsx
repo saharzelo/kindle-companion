336ba05099607b3459a79c93d9470255faa299ff
sahar <saharzelonagora@gmail.com>
2023-04-10 21:03:27 +0300

    refactored  BookInfoTable

--- packages/renderer/src/view/components/BookInfoTable/BookInfoTable.jsx
+++ packages/renderer/src/view/components/BookInfoTable/BookInfoTable.jsx
@@ -1,50 +1,44 @@
 import { useState } from 'react';
-import './BookInfoTable.css'
+import './BookInfoTable.css';
 
 function BookInfoTable({ tableData, tableHeaders }) {
     const [data, setData] = useState(tableData);
 
-    const removeData = (id) => {
-        const deletedItem = data.filter((item) => id !== item.id);
-        setData(deletedItem);
+    const handleDelete = (item) => {
+        const newData = data.filter((dataItem) => dataItem !== item);
+        setData(newData);
     };
 
     const renderHeaders = () => {
-        return tableHeaders.map((header, index) => {
-            return <th key={index}>{header.toUpperCase()}</th>;
-        });
+        return tableHeaders.map((header, index) => <th key={index}>{header.toUpperCase()}</th>);
     };
 
     const renderBody = () => {
-        if (data.length === 0 ) {
+        if (data.length === 0) {
             return null;
         }
-        const headerKeys = Object.keys(data[0]);
-        return data.map((item, index) => {
-            return (
-                <tr key={index}>
-                    {headerKeys.map((key) => (
-                        <td key={key}>{item[key]}</td>
-                    ))}
-                    <td className="operation">
-                        <button className="button" onClick={() => removeData(item.id)}>
-                            Delete
-                        </button>
-                    </td>
-                </tr>
-            );
-        });
+
+        return data.map((item, index) => (
+            <tr key={index}>
+                {Object.keys(item).map((key) => (
+                    <td key={key}>{item[key]}</td>
+                ))}
+                <td className="operation">
+                    <button className="button" onClick={() => handleDelete(item)}>
+                        Delete
+                    </button>
+                </td>
+            </tr>
+        ));
     };
 
     return (
-        <>
-            <table className="book-info-table">
-                <thead>
-                    <tr>{renderHeaders()}</tr>
-                </thead>
-                <tbody>{renderBody()}</tbody>
-            </table>
-        </>
+        <table className="book-info-table">
+            <thead>
+                <tr>{renderHeaders()}</tr>
+            </thead>
+            <tbody>{renderBody()}</tbody>
+        </table>
     );
 }
 
