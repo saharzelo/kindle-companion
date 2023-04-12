8f97289b06f324bb6a0060d4fa72955aa7bfd263
sahar <saharzelonagora@gmail.com>
2023-04-12 13:57:46 +0300

    testing repo functinos

--- packages/preload/src/index.js
+++ packages/preload/src/index.js
@@ -2,7 +2,18 @@
  * @module preload
  */
 
+import { findLatestWordDate } from './database/repository/wordRepository';
+import { findBooksByDate } from './database/repository/bookInfoRepository';
+import { findLookupsByDate } from './database/repository/lookupRepository';
 
+const getMe = async () => {
+    const date = await findLatestWordDate()
+    console.log(typeof(date.latest_date))
+    console.log(await findBooksByDate(date.latest_date))
+    console.log(await findLookupsByDate(date.latest_date))
+
+}
+getMe()
 export { findAllWords, findLookupsByAsin } from './database/repository/lookupRepository'
 export { findAllBooks, findBookByAsin } from './database/repository/bookInfoRepository'
 export { getThumbnails } from './ipc/getThumbnails';
