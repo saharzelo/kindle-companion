82a89fd068bd08f04177def77a813ae4682da5d4
sahar <saharzelonagora@gmail.com>
2023-04-12 13:15:59 +0300

    add function to retrieve latest word date

---
+++ packages/preload/src/database/repository/wordRepository.js
@@ -0,0 +1,20 @@
+import { getConnection } from '../createConnection'
+
+
+export function findLatestWordDate() {
+    const con = getConnection();
+    const query = `
+        SELECT 
+            date(MAX(timestamp) / 1000, 'unixepoch') AS latest_date 
+        FROM 
+            WORDS;`;
+    return new Promise((resolve, reject) => {
+        con.all(query, [], (err, rows) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(rows);
+            }
+        });
+    });
+}
