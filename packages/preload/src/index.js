0c5d1d1071682b3dd5d5b4e94fd869dc4c2e2d8d
sahar <saharzelonagora@gmail.com>
2023-04-12 15:18:25 +0300

    feat: Add ability to view books and defined words from last reading session on homepage

--- packages/preload/src/index.js
+++ packages/preload/src/index.js
@@ -8,13 +8,19 @@ import { findLookupsByDate } from './database/repository/lookupRepository';
 
 const getMe = async () => {
     const date = await findLatestWordDate()
-    console.log(typeof(date.latest_date))
+    console.log(typeof (date.latest_date))
     console.log(await findBooksByDate(date.latest_date))
     console.log(await findLookupsByDate(date.latest_date))
 
 }
 getMe()
-export { findAllWords, findLookupsByAsin } from './database/repository/lookupRepository'
+
+
+
+export { findLatestWordDate } from './database/repository/wordRepository';
+export { findBooksByDate } from './database/repository/bookInfoRepository';
+export { findLookupsByDate } from './database/repository/lookupRepository';
+export { findLookupsByAsin } from './database/repository/lookupRepository'
 export { findAllBooks, findBookByAsin } from './database/repository/bookInfoRepository'
 export { getThumbnails } from './ipc/getThumbnails';
 export { exportKindleContent } from './ipc/exportKindleContent';
--- packages/renderer/src/view/pages/HomePage/HomePage.jsx
+++ packages/renderer/src/view/pages/HomePage/HomePage.jsx
@@ -1,7 +1,48 @@
 import './HomePage.css'
+import { useState, useEffect } from 'react';
+import {
+    findLatestWordDate,
+    findBooksByDate,
+    findLookupsByDate,
+    getThumbnails
+} from '#preload'
+import BooksCatalog from "../../components/BooksCatalog/BooksCatalog";
+import BookInfo from "../../components/BookInfoModal/BookInfoModal";
+
 
 
 function HomePage({ }) {
+    const [books, setBooks] = useState([]);
+    const [selectedBookAsin, setSelectedBookAsin] = useState(null);
+    const [thumbnails, setThumbnails] = useState(null);
+    const [showModal, setShowModal] = useState(false);
+    const [searchQuery, setSearchQuery] = useState("");
+
+    const handleBookClick = (asin) => {
+        setSelectedBookAsin(asin);
+        setShowModal(true);
+    };
+
+
+    useEffect(() => {
+        async function prepKindleData() {
+            try {
+                const date = await findLatestWordDate();
+                const books = await findBooksByDate(date.latest_date)
+                console.log(books)
+                const bookId = books.map((result) => result.asin);
+                const thumbnails = await getThumbnails(bookId);
+                setThumbnails(thumbnails);
+                setBooks(books);
+            } catch (error) {
+                console.error("error loading book catalog items, error:\n", error);
+            }
+        }
+        prepKindleData();
+    }, []);
+
+
+
     return (
         <div className="home-page">
             <div className="homepage-header">
@@ -39,9 +80,24 @@ function HomePage({ }) {
                     <div className="choose-table-buttons">
                         <h3> Books </h3> <h3> Words </h3>  <h3> Clippings </h3>
                     </div>
+                    <div className="homepage-catalog">
+                        <BooksCatalog
+                            books={books}
+                            thumbnails={thumbnails}
+                            onBookClick={handleBookClick}
+                        />
+
+                        {showModal && (
+                            <BookInfo
+                                bookAsin={selectedBookAsin}
+                                setShowModal={setShowModal}
+                                thumbnail={thumbnails[selectedBookAsin]}
+                            />
+                        )}
 
 
 
+                    </div>
 
                 </div>
 
