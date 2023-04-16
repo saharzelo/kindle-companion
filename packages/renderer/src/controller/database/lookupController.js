95c653f51c624b177992966d0533f714831667c0
sahar <saharzelonagora@gmail.com>
2023-04-16 18:12:57 +0300

    add getLookupsByAsin() db call

--- packages/renderer/src/controller/database/lookupController.js
+++ packages/renderer/src/controller/database/lookupController.js
@@ -1,7 +1,7 @@
 import { lookupRepo } from '#preload';
 
 
-export async function getLookupsByDate() {
+export async function getRecentLookups() {
     const date = await lookupRepo.GetLatestLookupDate()
     const books = await lookupRepo.getLookupsByDate(date.latest_date)
     return books
@@ -11,6 +11,16 @@ export async function getLookupsByDate() {
 export async function getAllLookups() {
     try {
         const books = 1
+        return books
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+
+export async function getLookupsByAsin(asin) {
+    try {
+        const books = lookupRepo.getLookupsByAsin(asin)
         return books
     } catch (error) {
         console.error(error)
