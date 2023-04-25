cd5feb3b36d8fd50da11de1342ece0e089c616c9
sahar <saharzelonagora@gmail.com>
2023-04-26 00:02:35 +0300

    add config setter and formatting

--- packages/main/src/config.js
+++ packages/main/src/config.js
@@ -1,20 +1,20 @@
 import { app } from "electron";
 import path from "path";
 
-const appName = "kindle-companion";
-const tmpDir = path.join(app.getPath("temp"), `${appName}`);
-const appDir = path.join(app.getPath("userData"), `${appName}`);
-
+const APPNAME = "kindle-companion";
+const TMPDIR = path.join(app.getPath("temp"), `${APPNAME}`);
+const APPDIR = path.join(app.getPath("userData"), `${APPNAME}`);
 
 const defaultConfig = {
-    tmpDir: tmpDir,
-    appDir: appDir,
+    tmpDir: TMPDIR,
+    appDir: APPDIR,
+    profileDir: path.join(APPDIR, "profiles"),
+    vocabFilePath: path.join("system", "vocabulary", "vocab.db")
 };
 
 export function getConfig() {
     return defaultConfig;
 }
-
-export function setConfig() {}
-
-export default defaultConfig;
+export function setConfig(newConfig) {
+    defaultConfig = { ...defaultConfig, ...newConfig };
+}
