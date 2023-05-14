bce5e8f7d77aab555a8e049b470a813bfcaa2256
sahar <saharzelonagora@gmail.com>
2023-05-14 21:38:42 +0300

    add wordsPage logic

--- packages/renderer/src/view/pages/WordsPage/WordsPage.jsx
+++ packages/renderer/src/view/pages/WordsPage/WordsPage.jsx
@@ -1,17 +1,43 @@
 import WordsCatalog from "../../components/WordsCatalog/WordsCatalog";
+import { prepLookupData } from "../../../controller/services/lookupService";
+import LoadingScreen from "../../components/LoadingScreen/LoadingScreen";
+import WordInfoModal from "../../components/WordInfoModal/WordInfoModal";
 import { useState, useEffect } from "react";
 import "./WordsPage.css";
 
 function WordsPage() {
-    const [wordsData, setWordsData] = useState();
+    const [lookupData, setLookupData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [searchQuery, setSearchQuery] = useState("");
+    const [selectedWord, setSelectedWord] = useState("");
+    const [showWordModal, setShowWordModal] = useState(false);
 
     useEffect(() => {
         async function fetchData() {
-            setWordsData()
+            const lookups = await prepLookupData()
+            setLookupData(lookups)
+            setLoading(false)
         }
         fetchData();
     }, []);
-                       
+
+
+    const handleWordClick = (word) => {
+        setSelectedWord(word);
+        setShowWordModal(true);
+    };
+
+    const handleSearchInputChange = (event) => {
+        setSearchQuery(event.target.value);
+    };
+
+    const filteredBooks = lookupData.filter((lookup) =>
+        lookup.word.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+
+    if (loading) {
+        return <LoadingScreen />;
+    }
 
     return (
         <div className="words-page">
@@ -20,10 +46,21 @@ function WordsPage() {
                 <input
                     type="text"
                     placeholder="Search..."
-                    // value={searchQuery}
-                    // onChange={handleSearchInputChange}
+                    value={searchQuery}
+                    onChange={handleSearchInputChange}
                 />
             </div>
+            <WordsCatalog
+                words={filteredBooks}
+                onWordClick={handleWordClick}
+            />
+
+            {showWordModal && (
+                <WordInfoModal
+                    word={selectedWord}
+                    setShowModal={setShowWordModal}
+                />
+            )}
 
         </div>
     );
